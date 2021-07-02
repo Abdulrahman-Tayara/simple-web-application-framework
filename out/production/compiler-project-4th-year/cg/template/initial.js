bc0cbba8a6900e90d8918b8124d672037f991274
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2021-07-02 16:07:48 +0300

    have to commit

---
+++ out/production/compiler-project-4th-year/cg/template/initial.js
@@ -0,0 +1,12 @@
+let inits = [];
+let changes = [];
+let forRenders = [];
+
+let defaultInnerHTML = {};
+let cpShowIdBindings = {};
+let cpHideIdBindings = {};
+let cpIfConditionalBindings = [];
+let cpSwitchBindings = [];
+
+let deletedNodes = [];
+let repeatedNodes = [];
